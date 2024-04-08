63537227018893a68b9c4cf180ee25a021e293b8
kme-developer <electruc0095@gmail.com>
2024-04-08 13:34:33 +0900

    build. default setting.

---
+++ src/init.js
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+import Http from 'node:http';
+import { ExpressApp } from './app';
+import { serverCache } from './cache';
+
+dotenv.config();
+
+export class Server {
+  expressApp = new ExpressApp();
+  httpServer;
+
+  constructor() {
+    this.httpServer = new Http.Server(this.expressApp.app);
+  }
+
+  runServer = async () => {
+    try {
+      await this.databaseConnection();
+      await serverCache.preCachingOptions();
+      return this.serverListen();
+    } catch (error) {
+      return this.serverErrorHandler(error);
+    }
+  };
+
+  serverListen = () => {
+    const { PORT: port, HOST: host } = process.env;
+    return this.httpServer.listen(port, () => {
+      console.log(`Server is running on: http://${host}:${port}`);
+    });
+  };
+
+  serverErrorHandler = (error) => {
+    console.log('Server run error: ', error.message);
+  };
+}
+
+const server = new Server();
+
+server.runServer();
